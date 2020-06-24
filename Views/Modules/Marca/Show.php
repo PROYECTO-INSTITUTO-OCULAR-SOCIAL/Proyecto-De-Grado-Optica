59e121252cb46b2d78a21160e21a2762a88277a1
HAMILTON FONSECA <hrfonseca3@misena.edu.co>
2020-06-23 22:05:31 -0500

    commit tabla marca

---
+++ Views/Modules/Marca/Show.php
@@ -0,0 +1,116 @@
+<?php
+require("../../Partials/Routes.php");
+require("../../../app/Controllers/MarcaController.php");
+
+use App\Controllers\MarcaController; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= getenv('TITLE_SITE') ?> | Datos de Marca</title>
+    <?php require("../../Partials/Head_Imports.php"); ?>
+</head>
+<body class="hold-transition sidebar-mini">
+
+<!-- Site wrapper -->
+<div class="wrapper">
+    <?php require("../../Partials/Navbar_Customization.php"); ?>
+
+    <?php require("../../Partials/Sliderbar_Main_Menu.php"); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Información de Marca</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="<?= $baseURL; ?>/Views/">Proyecto-De-Grado-Optica</a></li>
+                            <li class="breadcrumb-item active">Inicio</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <?php if(!empty($_GET['respuesta'])){ ?>
+                <?php if ($_GET['respuesta'] == "error"){ ?>
+                    <div class="alert alert-danger alert-dismissible">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                        <h5><i class="icon fas fa-ban"></i> Error!</h5>
+                        Error al consultar Marca: <?= ($_GET['mensaje']) ?? "" ?>
+                    </div>
+                <?php } ?>
+            <?php } else if (empty($_GET['id_marca'])) { ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <h5><i class="icon fas fa-ban"></i> Error!</h5>
+                    Faltan criterios de busqueda <?= ($_GET['mensaje']) ?? "" ?>
+                </div>
+            <?php } ?>
+
+            <!-- Horizontal Form -->
+            <div class="card card-info">
+                <?php if(!empty($_GET["id_marca"]) && isset($_GET["id_marca"])){
+                    $DataMarca = MarcaController::searchForid_marca($_GET["id_marca"]);
+                    if(!empty($DataMarca)){
+                        ?>
+                        <div class="card-header">
+                            <h3 class="card-title"><?= $DataMarca->getnombre()  ?></h3>
+                        </div>
+                        <div class="card-body">
+                            <p>
+
+
+                                <strong><i class="fas fa-book mr-1"></i>Nombre</strong>
+                            <p class="text-muted">
+                                <?= $DataMarca->getnombre()?>
+                            </p>
+                            <hr>
+                            <strong><i class="far fa-file-alt mr-1"></i> Estado</strong>
+                            <p class="text-muted"><?= $DataMarca->getestado() ?></p>
+                            </p>
+
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-auto mr-auto">
+                                    <a role="button" href="index.php" class="btn btn-success float-right" style="margin-right: 5px;">
+                                        <i class="fas fa-tasks"></i> Gestionar Marca
+                                    </a>
+                                </div>
+                                <div class="col-auto">
+                                    <a role="button" href="Create.php" class="btn btn-primary float-right" style="margin-right: 5px;">
+                                        <i class="fas fa-plus"></i> Crear Marca
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php }else{ ?>
+                        <div class="alert alert-danger alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            <h5><i class="icon fas fa-ban"></i> Error!</h5>
+                            No se encontro ningun registro con estos parametros de busqueda <?= ($_GET['mensaje']) ?? "" ?>
+                        </div>
+                    <?php }
+                } ?>
+            </div>
+            <!-- /.card -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <?php require ('../../Partials/Footer.php');?>
+</div>
+<!-- ./wrapper -->
+<?php require ('../../Partials/Scripts.php');?>
+</body>
+</html>
+
