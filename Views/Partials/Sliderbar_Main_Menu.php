d5587f690d01a41c8d59811c69e5439c384cac23
Hildebrando <hyunado@misena.edu.co>
2020-06-25 19:09:51 -0500

    Creacion de Deploymen

--- Views/Partials/Sliderbar_Main_Menu.php
+++ Views/Partials/Sliderbar_Main_Menu.php
@@ -8,7 +8,7 @@
              alt="AdminLTE Logo"
              class="brand-image img-circle elevation-3"
              style="opacity: .8">
-        <span class="brand-text font-weight-light">Proyecto-De-Grado-Optica</span>
+        <span class="brand-text font-weight-light">Proyecto De Grado Optica</span>
     </a>
 
     <!-- Sidebar -->
@@ -17,11 +17,10 @@
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
 
-                <img src="<?= $baseURL ?>/views/components/img/user.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?= $baseURL ?>/views/components/img/123.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Diana Chaparro</a>
-              master
+                <a href="#" class="d-block">Hildebrando Yunado Vergara</a>
             </div>
         </div>
         <!-- Sidebar Menu -->
@@ -30,7 +29,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="<?= $baseURL; ?>/views/" class="nav-link">
+                    <a href="<?= $baseURL; ?>/Views/" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Inicio
@@ -43,31 +42,31 @@
                     <a href="#" class="nav-link active">
                         <i class="nav-icon far fa-user"></i>
                         <p>
-                            Categoria
+                            Municipio
                             <i class="fas fa-angle-left right"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="<?= $baseURL ?>/views/Modules/Categoria/Create.php" class="nav-link">
+                            <a href="<?= $baseURL ?>/Views/Modules/Municipio/Create.php" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Crear</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="<?= $baseURL ?>/views/Modules/Categoria/Edit.php" class="nav-link">
+                            <a href="<?= $baseURL ?>/Views/Modules/Municipio/Edit.php" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Editar</p>
                             </a>
                         </li>
                        <li class="nav-item">
-                            <a href="<?= $baseURL ?>/views/modules/usuarios/create.php" class="nav-link">
+                            <a href="<?= $baseURL ?>/Views/Modules/Municipio/index.php" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>index</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="<?= $baseURL ?>/views/modules/usuarios/create.php" class="nav-link">
+                            <a href="<?= $baseURL ?>/Views/Modules/Municipio/Show.php" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Show</p>
                             </a>
